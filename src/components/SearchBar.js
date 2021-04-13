7d68a25ae4919f52cacfbf14a652879d4161fffd
YusukeYoshihiro <yusuke.10.25.61@gmail,com>
2021-04-12 23:17:14 -0700

    Add Styled-components     adding the SR only styling for label with styled-components on src/components/SearchBar.js

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,4 +1,19 @@
-import React from 'react'
+import React from 'react';
+import styled from 'styled-components';
+
+const Label = styled.label`
+  border: 0 !important;
+	clip: rect(1px, 1px, 1px, 1px) !important; /* 1 */
+	-webkit-clip-path: inset(50%) !important;
+		clip-path: inset(50%) !important;  /* 2 */
+	height: 1px !important;
+	margin: -1px !important;
+	overflow: hidden !important;
+	padding: 0 !important;
+	position: absolute !important;
+	width: 1px !important;
+	white-space: nowrap !important;            /* 3 */
+`
 
 export default class SearchBar extends React.Component {
   state = { term: "" };
@@ -21,12 +36,13 @@ export default class SearchBar extends React.Component {
       <div className="search-bar ui segment">
         <form onSubmit={this.onFormSubmit} className="ui form">
           <div className="field">
-            <label>Search Video</label>
+            <Label>Search Bar</Label>
             <input
               type="text"
               value={this.state.term}
               // onChange={e=> this.setState({term: e.target.value})}
               onChange={this.onInputChange}
+              placeholder="Search Video"
             />
           </div>
         </form>
