de4c9ffebfb9fe561e76f449250515857c55b8d6
YusukeYoshihiro <yusuke.10.25.61@gmail,com>
2021-04-14 03:48:35 -0700

    Added viewsCount and publishedAt(Date)   Added viewCount and pubilishedAt info to VideoDetail.js and   Added publish from Now info to VideoItems.js

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -2,13 +2,25 @@ import React from 'react';
 import VideoItem from './VideoItem';
 
 // props.videos = videos
-const VideoList = ({ videos, onVideoSelect }) => {
-  const renderedList = videos.map((video) => {
+const VideoList = ({ videos, onVideoSelect, viewItemCount}) => {
+  const views = Number(viewItemCount);
+
+  const numFormatter = (num) => {
+    if (num > 999 && num < 1000000) {
+      return (num / 1000).toFixed(1) + 'K';
+    } else if (num > 1000000) {
+      return (num / 1000000).toFixed(1) + 'M';
+    } else if (num < 900) {
+      return num;
+    }
+  }
+  const renderedList = videos.map((video ) => {
     return (
       <VideoItem
         onVideoSelect={onVideoSelect}
         video={video}
         key={video.id.videoId}
+        numFormatter={numFormatter(views)}
       />
     )
   })
